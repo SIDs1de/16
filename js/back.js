bf9f7ad94368321d64f68a7bd36801cebbea9b3a
SIDs1de <82277183+SIDs1de@users.noreply.github.com>
2022-10-08 18:04:23 +0300

    Add files via upload

---
+++ js/back.js
@@ -0,0 +1,60 @@
+var promo_name = '';
+var price = 18490;
+var linkwithout = "https://t.me/Brainuniversity_bot?start=nopromo_course_29"
+document.querySelectorAll("#nopromo_link").forEach((ah) => {
+    ah.href = linkwithout;
+})
+function send_ajax(){
+    if (promo_name != ''){
+        $.ajax({
+            url: "https://api.brainuniversity.ru/checkpromocourse/29&"+ promo_name,
+            type: "POST",
+            data: {'res': 'res'},
+            success: function(resp){
+                if (resp['res'] == 'success'){
+                    var discount = resp["discount"]
+                    var link = resp["return_link"]
+                    document.getElementById("popup1").classList.remove("active");
+                    document.getElementById("popup2").classList.add("active");
+                    var newprice = price * (100 - discount) / 100;
+                    document.getElementById("promo_price").innerHTML = newprice.toFixed(2) + "&nbsp;руб";
+                    document.getElementById("promo_sent").onclick = function(){ gotg(link) };
+                    document.querySelectorAll("#promo_link").forEach((ah) => {
+                        ah.href = link;
+                    })
+                }
+                else {
+                    document.getElementById("popup1").classList.remove("active");
+                    document.getElementById("popup3").classList.add("active");
+                    promo_name = '';
+                }
+            },
+            error: function(resp){
+                console.log(resp)
+            }
+        })
+    }
+}
+function eneter_promo1(){
+    newprom = document.getElementById("promo_enter1").value;
+    if (newprom.length > 0){
+        promo_name = newprom;
+        send_ajax();
+    }
+}
+
+function withoutpromo(){
+    document.getElementById("popup1").classList.remove("active");
+    document.getElementById("popup3").classList.remove("active");
+    document.getElementById("popup4").classList.add("active")
+}
+function gotgbut(){
+    document.location.href = linkwithout;
+}
+function gotg(link){
+    document.location.href = link
+}
+function tryagain(){
+    document.getElementById("popup1").classList.add("active");
+    document.getElementById("popup3").classList.remove("active");
+}
